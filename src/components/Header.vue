b7215c0bfddfe8116b0b57a3fd4939db31e08326
Anderson Filipe <anderson.lemobs@gmail.com>
2021-06-17 16:00:34 -0300

    Primeiro commit

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app-bar
+    app
+    color="secondary"
+    dark
+  >
+    <div class="d-flex align-center ml-6">
+      <h2>Github Search</h2>
+    </div>
+  </v-app-bar>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: "Header"
+})
+</script>
---
+++ src/service/axios.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const baseURL = "https://api.github.com/";
+const params = {
+  client_id: "1d011406efab02a39427",
+  client_secret: "da2f13b80002a40c3876902b0812a7c3919a72cc"
+};
+
+const api = axios.create({
+  baseURL,
+  params
+});
+
+export { api };
